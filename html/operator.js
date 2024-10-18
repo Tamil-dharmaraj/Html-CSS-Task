9d2080d38f2fee7f6a56311d3f8c55b8fadeb4c1
TamilVanan <dharmatamil141@gmail.com>
2024-10-18 12:59:16 +0530

    first commit

---
+++ html/operator.js
@@ -0,0 +1,29 @@
+let a=10;
+let b=5;
+let c='8';
+let d=20;
+//Arithmetic operator
+console.log('a+b:'+(a+b));
+console.log('a-b:'+(a-b));
+console.log('a*b:'+(a*b));
+console.log('a%b:'+(a%b));
+console.log('a/b:'+(a/b));
+//Assignment operator
+console.log('a+=:'+(a+=30));
+console.log('a-=:'+(a-=3));
+console.log('a*=:'+(a*=20));
+console.log('a%=:'+(a%=10));
+//comparison operator
+console.log('c==d:'+(c==d));
+console.log('c==-d:'+(c===d));
+console.log('c!=d:'+(c!=d));
+console.log('c!==d:'+(c!==d));
+console.log('a<b:'+(a<b));
+console.log('a>b:'+(a>b));
+console.log('a<=b:'+(a<=b));
+console.log('a>=b:'+(a>=b));
+//Logical operator
+console.log('a>b&&c==d:'+(a>b&&c==d));
+console.log('a<b&&c==d:'+(a<b&&c==d));
+console.log('!(a>b):'+!a>b);
+//Ternary operator
